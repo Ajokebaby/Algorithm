42a4a082168e72a50216b1950975c42372e28da8
sekinah <sekinahdeleoladepo@gmail.com>
2025-04-16 12:55:33 +0100

    Multiplication Table

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -11,11 +11,21 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
+            //3 multiplication table
+            Console.Write("Enter a number: ");
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 12; i++)
+            {
+               
+                    Console.WriteLine($"multiplication table {N}: {N}*{i} {N * i} ");
+
+            }
+
             //4
             //Console.WriteLine("write the range:");
             //int range = int.Parse(Console.ReadLine());
-           
-            //for (int i = 0; i <= range; i += 2) 
+
+            //for (int i = 0; i <= range; i += 2)
             //{
             //    if (range % 2 == 0)
             //    {
@@ -25,14 +35,15 @@ namespace Algorithm
             //    {
             //        continue;
             //    }
-                
+
             //}
             //5 square
-            for (int i = 1; i <= 10; i++) 
-            {
+            //for (int i = 1; i <= 10; i++) 
+            //{
                 
-                Console.WriteLine($"{i} squared is {i * i}");
-            }
+            //    Console.WriteLine($"{i} squared is {i * i}");
+            //}
+            //
            
         }
        
