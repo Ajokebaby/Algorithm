8f90c59991d800dce3e125de4d8f84b946f0a910
sekinah <sekinahdeleoladepo@gmail.com>
2025-04-16 13:33:25 +0100

    sum

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -11,41 +11,53 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
+            //1 sum
+            Console.WriteLine("Enter the number");
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                sum += i; 
+            }
+
+            Console.WriteLine($"The sum of the first {num} natural numbers is: {sum}");
+
             //3 multiplication table
             Console.Write("Enter a number: ");
-            int N = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
             for (int i = 1; i <= 12; i++)
             {
                
-                    Console.WriteLine($"multiplication table {N}: {N}*{i} {N * i} ");
+                    Console.WriteLine($"multiplication table {n}: {n}*{i} {n * i} ");
 
             }
 
             //4
-            //Console.WriteLine("write the range:");
-            //int range = int.Parse(Console.ReadLine());
+            Console.WriteLine("write the range:");
+            int range = int.Parse(Console.ReadLine());
 
-            //for (int i = 0; i <= range; i += 2)
-            //{
-            //    if (range % 2 == 0)
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //    else
-            //    {
-            //        continue;
-            //    }
+            for (int i = 0; i <= range; i += 2)
+            {
+                if (range % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    continue;
+                }
 
-            //}
+            }
             //5 square
-            //for (int i = 1; i <= 10; i++) 
-            //{
-                
-            //    Console.WriteLine($"{i} squared is {i * i}");
-            //}
+            for (int i = 1; i <= 10; i++)
+            {
+
+                Console.WriteLine($"{i} squared is {i * i}");
+            }
             //
-           
+
         }
-       
+
     }
 }
